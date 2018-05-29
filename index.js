ad1011add35463af280765cb62d3ecea73aaea03
hanakolc <hanako_cetin27@yahoo.fr>
2018-05-30 00:17:11 +0200

    feat(questions): save question 1 to 10

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 // Q1. Create a variable called `kanye` and assign it the string 'please kanye, help me'
+var kanye = 'please kanye, help me';
 
 // Q2. Use the variable `artist` to create a new variable called `genious` that is assigned the string "Is Kanye West a musical genius?"
 const artist = 'Kanye West';
+var genious = 'Is '+artist+' a musical genius?';
 
 // Q3  Make an object called `family` and give it the following properties:
 // family has been `married` on 2014 (Number)
 // family is not presidentOfTheUnitedStates (boolean)
 // family has `children` called North, Saint and Chicago (array of strings)
+var family = {
+	'married' : 2014,
+	'presidentOfTheUnitedStates' : true,
+	'children' : ['North', 'Saint', 'Chicago']
+}; 
 
 // Q4. Change the `brand` shoes to "adidas" using dot notation
 const shoes = {
   'name': 'yeezy',
   'brand': 'nike'
 };
+shoes.brand = 'adidas';
 
 // Q5. Change the `model` yeezy to "Super Moon Yellow" using square bracket notation
 const yeezy = {
   'model': '350'
 };
+yeezy['model'] = 'Super Moon Yellow';
 
 // Q6. Create an object call "discography"
 // The key should be the album name and the value the release year
@@ -27,28 +36,50 @@ const yeezy = {
 // * Graduation (2007)
 // * 808s & Heartbreak (2008)
 // * My Beautiful Dark Twisted Fantasy (2010)
+var discography = {
+	'The College Dropout' : 2004,
+	'Late Registration' : 2005,
+	'Graduation' : 2007,
+	'808s & Heartbreak' : 2008,
+	'My Beautiful Dark Twisted Fantasy' : 2010
+};
 
 // Q7. Return a new array from `stronger` with all item in uppercase
 const stronger = ['Work it', 'make it', 'do it', 'makes us', 'Harder', 'better', 'faster', 'stronger'];
+const strongerUpper = stronger.map(function toUpper(item) {
+    return item.toUpperCase();
+});
 
 // Q8. Using this array do the following
 const friends = ['jayz', 'rihanna', 'kidcudi'];
 
 // 1. add 'drake' to the end of `friends` array
-
+friends.push('drake');
 // 2. remove 'jayz' and store him in a variable called firstFriend
-
+var indexJZ = array.indexOf('jayz');
+var firstFriend = friends[indexJZ];
+if (index > -1) {
+  friends.splice(indexJZ, 1);
+}
 // 3. add 'pusha-t' to the start of `friends` array
-
+friends.unshift("pusha-t");
 // 4. remove 'rihanna' from the array and store him in a variable called secondFriend
-
+var indexRiri = array.indexOf('rihanna');
+var secondFriend = friends[indexRiri];
+if (index > -1) {
+  friends.splice(indexRiri, 1);
+}
 // 5. leave 'kidcudi' in the array but put a copy of him on a variable called thirdFriend
-
+var indexKC = array.indexOf('kidcudi');
+var thirdFriend = friends[indexKC];
 
 // Q9. Write the function `duplicate` that return the expected result
 console.log(duplicate(['let', 'the', 'suicide', 'doors', 'up']));
 // ❯ ['let', 'the', 'suicide', 'doors', 'up', 'let', 'the', 'suicide', 'doors', 'up']
-
+function duplicate(myArray) {
+	var clone = myArray.slice(0);
+    return myArray.concat(clone) ;           
+}
 
 // Q10. Refactor the current ES5-style function `heartless` in ES6-style.
 
@@ -66,10 +97,17 @@ const chorus = ['He lost his soul to a woman so ', 'How could you be so ', 'Oh, 
 console.log(heartless(chorus));
  // ❯ ['He lost his soul to a woman so heartless', 'How could you be so heartless', 'Oh, how could you be so heartless'];
 
+const heartless = (names) => {
+	const results = [];
+	for (let i in names) {
+  		results.push(names[i] + 'heartless');
+	}
+    return results;
+};
 
 // Q11. Related to the https://genius.com/search?q=kanye%20west link
 // What's the query selector to get all lyrics cards.
-
+mini_card-lyric_excerpt
 // Q12. Related to https://genius.com/search?q=kanye%20west link
 // List all api requests urls with their http action verbs.
 
